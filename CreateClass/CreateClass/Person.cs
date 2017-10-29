28693d7f1a416120567db0b09499a87f1c7d0d66
Alex Takács <alextakacs10@gmail.com>
2017-10-29 21:18:05 +0100

    Implemented Person class (exercise 1)

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        String Name;
+        String BirthDate;
+        public String Gender { get; set; }
+
+        enum Genders {Male, Female};
+
+        public Person(string name, String birthDate, string gender)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            if (Enum.IsDefined(typeof(Genders), gender))
+            {
+                Gender = gender;
+            } else
+            {
+                Gender = "Error";
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", BirthDate: " + BirthDate + ", Gender: " + Gender;
+        }
+    }
+}
