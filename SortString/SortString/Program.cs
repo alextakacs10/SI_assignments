b975ed8acdc097380dfb664505e61207963eb324
Alex Takács <alextakacs10@gmail.com>
2017-10-30 00:36:04 +0100

    Implemented SortString project (exercise 5)

---
+++ SortString/SortString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+
+            Array.Sort(words);
+
+            string sorted = string.Join(" ", words);
+            Console.WriteLine(sorted);
+            Console.ReadKey();
+
+        }
+    }
+}
