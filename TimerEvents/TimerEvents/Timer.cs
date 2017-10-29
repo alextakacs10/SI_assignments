ceebc33bd6300b1ddc247244dbf658db77bbcd14
Alex Takács <alextakacs10@gmail.com>
2017-10-30 00:29:12 +0100

    Implemented Timer (Exercise 4)

---
+++ TimerEvents/TimerEvents/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class Timer : Form
+    {
+        System.Windows.Forms.Timer t;
+
+        public Timer()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Load(object sender, EventArgs e)
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value += 10;
+            if (progressBar.Value >= 100) t.Stop();
+        }
+    }
+}
