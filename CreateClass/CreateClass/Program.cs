aac378eef7d3ca3196f20f922a3b77a783d173b9
Alex Takács <alextakacs10@gmail.com>
2017-10-29 21:17:49 +0100

    Created CreateClass project and implemented Program class

---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person alex = new Person("Alex", "1995.02.26", "Male");
+            Console.WriteLine(alex.ToString());
+
+            Person alexa = new Person("Alex", "1995.02.26", "Female");
+            Console.WriteLine(alexa.ToString());
+
+            Person unknown = new Person("Unknown", "1995.02.26", "Unknown");
+            Console.WriteLine(unknown.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
