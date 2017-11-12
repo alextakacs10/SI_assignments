d13743880c3110bf691d7e2ee1ac4774b9b7e716
Alex Takács <alextakacs10@gmail.com>
2017-11-12 18:30:07 +0100

    Implemented Search Pattern exercise.

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,46 @@ namespace SeekAndArchive
 {
     class Program
     {
+        static List<FileInfo> FoundFiles;
         static void Main(string[] args)
         {
+            string fileName = args[0];
+            string directoryName = args[1];
+            FoundFiles = new List<FileInfo>();
+
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+
+            if(!rootDir.Exists)
+            {
+                Console.WriteLine("Directory does not exist.");
+                return;
+            }
+
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+            Console.WriteLine("Found {0} files in {1} with pattern {2}.", FoundFiles.Count, rootDir.FullName, fileName);
+
+            foreach (FileInfo file in FoundFiles)
+            {
+                Console.WriteLine("{0}", file.FullName);
+            }
+            Console.ReadKey();
+        }
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo file in currentDirectory.GetFiles(fileName))
+            {
+                //if(file.Name == fileName)
+                //{
+                //    foundFiles.Add(file);
+                //}
+                foundFiles.Add(file);
+            }
+
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
         }
     }
 }
