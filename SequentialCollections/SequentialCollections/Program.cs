f25837e64c9670618fabe9ff3e048a709e6d1547
Alex Takács <alextakacs10@gmail.com>
2017-11-12 15:57:48 +0100

    Implemented W2E1

--- SequentialCollections/SequentialCollections/Program.cs
+++ SequentialCollections/SequentialCollections/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace SequentialCollections
 {
@@ -10,6 +11,18 @@ namespace SequentialCollections
     {
         static void Main(string[] args)
         {
+            Queue myQueue = new Queue();
+            myQueue.Enqueue("First");
+            myQueue.Enqueue("Second");
+            myQueue.Enqueue("Third");
+            myQueue.Enqueue("Fourth");
+
+            while (myQueue.Count > 0)
+            {
+                object obj = myQueue.Dequeue();
+                Console.WriteLine("Dequeued item: {0}", obj);
+            }
+            Console.ReadKey();
         }
     }
 }
