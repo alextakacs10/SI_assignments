4fb846db0c5ff91294e594fcb43960b7b9939fc5
Alex Takács <alextakacs10@gmail.com>
2017-11-12 16:17:42 +0100

    Implemented W2E1E3

--- DictionaryCollections/DictionaryCollections/Program.cs
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace DictionaryCollections
 {
@@ -10,6 +11,28 @@ namespace DictionaryCollections
     {
         static void Main(string[] args)
         {
+            Hashtable table = new Hashtable();
+            table["0"] = "Zero";
+            table["1"] = "One";
+            table["2"] = "Two";
+            table["3"] = "Three";
+            table["4"] = "Four";
+            table["5"] = "Five";
+            table["6"] = "Six";
+            table["7"] = "Seven";
+            table["8"] = "Eight";
+            table["9"] = "Nine";
+
+            String myString = "8276123";
+
+            foreach(char c in myString){
+                String character = c.ToString();
+                if (table.Contains(character))
+                {
+                    Console.WriteLine("Value for {0}: {1}", character, table[character]);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
