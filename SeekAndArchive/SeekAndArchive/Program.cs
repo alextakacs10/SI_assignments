68807ea24cc4c0199f6c5ca210423d310900632a
Alex Takács <alextakacs10@gmail.com>
2017-11-12 19:20:40 +0100

    Implemented Read Isolated storage.

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace SeekAndArchive
 {
     class Program
     {
+        //static variables
         static List<FileInfo> FoundFiles;
+        static List<FileSystemWatcher> watchers;
+        static List<DirectoryInfo> archiveDirs;
+
         static void Main(string[] args)
         {
             string fileName = args[0];
@@ -34,8 +39,26 @@ namespace SeekAndArchive
             {
                 Console.WriteLine("{0}", file.FullName);
             }
+
+            watchers = new List<FileSystemWatcher>();
+
+            foreach (FileInfo file in FoundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(file.DirectoryName, file.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
+            }
+
+            archiveDirs = new List<DirectoryInfo>();
+            for (int i = 0; i < FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i.ToString()));
+            }
+
             Console.WriteLine("Pres any key to exit.");
             Console.ReadKey(true);
+
         }
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
@@ -49,6 +72,32 @@ namespace SeekAndArchive
             {
                 RecursiveSearch(foundFiles, fileName, dir);
             }
+        }
+
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("{0} has been changed!", e.FullPath);
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+            int index = watchers.IndexOf(senderWatcher, 0);
+            ArchiveFiles(archiveDirs[index], FoundFiles[index]);
+        }
+
+        static void ArchiveFiles(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
+            GZipStream compressor = new GZipStream(output, CompressionMode.Compress);
+
+            int b = input.ReadByte();
+
+            while(b != -1)
+            {
+                compressor.WriteByte((byte)b);
+                b = input.ReadByte();
+            }
+            compressor.Close();
+            input.Close();
+            output.Close();
         }
     }
 }
