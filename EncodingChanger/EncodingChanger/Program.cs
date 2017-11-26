c509c8c743a75667f2ab1edef611926ffeef3e71
Alex Takács <alextakacs10@gmail.com>
2017-11-26 17:40:08 +0100

    Implemented EncodingChanger

---
+++ EncodingChanger/EncodingChanger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EncodingChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader(@"C:\TEST\boot.txt");
+            StreamWriter streamWriter = new StreamWriter(@"C:\TEST\boot-utf7.txt", false, Encoding.UTF7);
+
+            streamWriter.WriteLine(streamReader.ReadToEnd());
+            streamWriter.Close();
+            streamReader.Close();
+        }
+    }
+}
