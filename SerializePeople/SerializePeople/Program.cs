d3b839d61483e599d2cae8920df2ec35555e8cba
Alex Takács <alextakacs10@gmail.com>
2017-11-26 18:28:51 +0100

    Implemented Serialize class

---
+++ SerializePeople/SerializePeople/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializePeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person alex = new Person("Alex", new DateTime(1995, 2, 26), Person.Genders.Male);
+            alex.Serialize(@"C:\TEST\myfile.bin");
+        }
+    }
+}
