2ff1221c83c7debcfedd499fc676455add0b0170
Alex Takács <alextakacs10@gmail.com>
2017-11-26 20:25:26 +0100

    Implemented Threading assignment.

---
+++ ThreadPoolDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.ReadKey();
+        }
+
+        static void ShowMyText(object state)
+        {
+            string text = "State: " + (string)state;
+            text += " Thread: " + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(text);
+        }
+    }
+}
