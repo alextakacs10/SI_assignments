3d8be200257c3cc10fa928fea96b37828197f821
Alex Takács <alextakacs10@gmail.com>
2017-12-12 01:11:19 +0100

    Implemented AssemblyDemoTwo

---
+++ AssemblyDemoTwo/AssemblyDemoTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyDemoTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\System.ServiceProcess.dll";
+
+            BindingFlags flags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+
+            Assembly theAssembly = Assembly.LoadFrom(path);
+            Console.WriteLine(theAssembly.FullName);
+            Type[] types = theAssembly.GetTypes();
+
+            foreach (Type t in types)
+            {
+                Console.WriteLine(" Type: {0}", t.Name);
+                MemberInfo[] members = t.GetMembers(flags);
+
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(" {0}: {1}", member.MemberType, member.Name);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
