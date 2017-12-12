43bc90d0f5c1aa54eec206e470194b11d22de83a
Alex Takács <alextakacs10@gmail.com>
2017-12-12 01:15:29 +0100

    Implemented DemoDynamic

---
+++ DemoDynamic/DemoDynamic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DemoDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName theName = new AssemblyName();
+            theName.Name = "DemoAssembly";
+            theName.Version = new Version("1.0.0.0");
+
+            AppDomain domain = AppDomain.CurrentDomain;
+
+            AssemblyBuilder assemBldr = domain.DefineDynamicAssembly(theName, AssemblyBuilderAccess.ReflectionOnly);
+
+            ModuleBuilder modBldr = assemBldr.DefineDynamicModule("CodeModule", "DemoAssembly.dll");
+
+            TypeBuilder animalBldr = modBldr.DefineType("Animal", TypeAttributes.Public);
+
+            Type animal = animalBldr.CreateType();
+            Console.WriteLine(animal.FullName);
+
+            foreach (MemberInfo m in animal.GetMembers())
+            {
+                Console.WriteLine(" Member ({0}): {1}", m.MemberType, m.Name);
+            }
+
+            Console.Read();
+        }
+    }
+}
