e2ae3c52090f0b39770d6c38fad34dfc06a33db5
Alex Takács <alextakacs10@gmail.com>
2017-12-12 01:13:45 +0100

    Implemented DynamicCodeDemo

---
+++ DynamicCodeDemo/DynamicCodeDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DynamicCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\System.Web.dll";
+
+            Assembly webAssembly = Assembly.LoadFile(path);
+
+            Type utilType = webAssembly.GetType("System.Web.HttpUtility");
+
+            MethodInfo encode = utilType.GetMethod("HtmlEncode", new Type[] { typeof(string) });
+            MethodInfo decode = utilType.GetMethod("HtmlDecode", new Type[] { typeof(string) });
+
+            string originalString = "This is Sally & Jack's Anniversary <sic>";
+
+            Console.WriteLine(originalString);
+
+            string encoded = (string)encode.Invoke(null, new object[] { originalString });
+
+            Console.WriteLine(encoded);
+
+            string decoded = (string)decode.Invoke(null, new object[] { encoded });
+
+            Console.WriteLine(decoded);
+            Console.Read();
+        }
+    }
+}
