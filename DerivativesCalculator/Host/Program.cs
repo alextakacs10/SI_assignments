e1333ff85af8676d74886e4df56d566d5fee0ac7
Alex Takács <alextakacs10@gmail.com>
2018-01-08 00:06:08 +0100

    Implemented until exercise 2

---
+++ DerivativesCalculator/Host/Program.cs
@@ -0,0 +1,20 @@
+﻿using DerivativeCalculatorService;
+using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type serviceType = typeof(Calculator);
+            using (ServiceHost host = new ServiceHost(serviceType))
+            {
+                host.Open();
+                Console.WriteLine("The derivatives calculator service is available.");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
